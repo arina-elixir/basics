21153775bceee253ca160f606e476b88820d5959
Godwin Udofia <udofiawin@gmail.com>
2019-11-04 14:29:34 +0100

    added guards (a bit ott)

--- app2.ex
+++ app2.ex
@@ -1,11 +1,19 @@
 defmodule App2 do
   def greet_everybody() do
     total = IO.gets("How many students do you have? ")
-    total = String.to_integer(total |> String.replace("\n", ""))
-    IO.puts(message(total))
+
+    try do
+      total = String.to_integer(total |> String.replace("\n", ""))
+      IO.puts(message(total))
+    rescue
+      e in ArgumentError ->
+        IO.puts("An error occurred. " <> e.message)
+        IO.puts("\nPlease try again with a number ")
+        greet_everybody()
+    end
   end
 
-  def message(total) do
+  def message(total) when is_integer(total) when is_float(total) do
     cond do
       total < 0 ->
         "You can not be owing students na."
